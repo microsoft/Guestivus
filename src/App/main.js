799183c2227800b17c75776c7eea9855e7d3b821
Geordie Moffatt <gemof@microsoft.com>
2019-06-07 10:25:35 -0700

    ADding logo and UI changes

--- src/App/main.js
+++ src/App/main.js
@@ -17,17 +17,20 @@ function createWindow () {
   // Create the browser window.
   mainWin = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 600,    
+    frame: false,
     webPreferences: {
       nodeIntegration: true      
     }
-  })
-
+  });
+  
   // and load the index.html of the app.
-  mainWin.loadFile('index.html')
+  mainWin.loadFile('index.html');
 
   // Open the DevTools.
   //mainWin.webContents.openDevTools()
+  //mainWin.setMenuBarVisibility(false);
+
 
   // Emitted when the window is closed.
   mainWin.on('closed', () => {
@@ -35,7 +38,7 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWin = null
-  })
+  });
 }
 
 // This method will be called when Electron has finished
